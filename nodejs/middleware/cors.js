83c8e722b4875aafee90e992a951eea813481014
MichaelGPerkins <michael.guy.perkins@gmail.com>
2020-09-25 08:38:40 -0400

    Fix bug which prevents sending of jwt by adding 'x-auth-token' to 'Allow-Headers' argument in CORS middleware.

--- nodejs/middleware/cors.js
+++ nodejs/middleware/cors.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "http://localhost:4200");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
     next();
 }
